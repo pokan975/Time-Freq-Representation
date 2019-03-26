3bcea8c6a9fdad1dacc0ee9aedcd4c2152dc8d36
pokan975 <44811052+pokan975@users.noreply.github.com>
2019-03-25 23:57:44 -0700

    Add files via upload

--- project_main.m
+++ project_main.m
@@ -2,44 +2,81 @@ clear
 close
 clc
 
-% alpha = 1;
-% fs = 1024;
-% t = (0:1/fs:1-1/fs)';
-% 
-% gau = exp(-1*alpha*(t.^2)/2);
-% gauh = hilbert(gau);
-% 
-% tfrstft(gauh);
+% T = 10e-3;
+fs = 51.2e3;
+% Ts = 1/fs;
+% t = Ts:Ts:T;
+ff = 1:1:512;
+t = 1:1:512;
+Nt = length(t);
 
-fs = 1024; Td = 2; 
-chirp_rate = 37.5; 
-fi1 = 110; fi2 = 30; 
+%% Signal 1 (Frequency-Overlapping Signal)
+t1 = t(75:130);
+t2 = t;
+t3 = t(245:445);
 
-x1 = fmlin(Td*fs, fi1/fs, (fi1 + 2*chirp_rate*Td)/fs); 
-x2 = fmlin(Td*fs, fi2/fs, (fi2 + 2*chirp_rate*Td)/fs); 
-x = x1 + x2;
+x1 = zeros(1,Nt);
+x1(75:130) = 2.7*exp(-1*((9 * (t1 - 102).^2) / 6272) + 1i*((2 * pi * 1000 * (t1 - 70).^2) / (511^2)));
+x2 = 1.4*exp(-1*(((t2 - 185).^2) / 338) + 1i*(2*pi*125*t2/511));
+x3 = zeros(1,Nt);
+x3(245:445) = exp(-1*(((t3 - 344).^2) / 80000) + 1i*(2*pi*125*t3/511));
 
-N = length(x); 
-h = gausswin(101); 
-tfr3 = tfrstft(x, 1:N, N, h);
-tfr3 = flipud(tfr3);
+x = (x1 + x2 + x3).';
 
-figure(1) 
-tt = linspace(0, Td, N);  
-ff = linspace(0, fs, N); 
-imagesc(tt, ff, abs(tfr3(1025:end,:)));
-% axis([0 Td 0 fs/2]); 
-% axis xy
-xlabel('Time (sec)'), ylabel('Frequency (Hz)') 
-title('STFT of the summed signal, low pass window size = 101')
+%% Signal 2 (Time-Overlapping Signal)
+t1 = t(100:400);
+t2 = t(125:375);
+t3 = t(200:300);
 
-% =========================================================================
-img = abs(tfr3);
-[row, col] = size(img);
-img_max = max(max(img));
-img_min = min(min(img));
+s1 = zeros(1,Nt);
+s1(100:400) = 0.5*exp(-1*(((t1 - 250).^2) / 125000) + 1i*(2*pi*30*t1/511));
+s2 = zeros(1,Nt);
+s2(125:375) = 0.8*exp(-1*((9*(t2 - 250).^2) / 781250) + 1i*((pi*4000*(t2 - 250).^3) / (3*511^3)) + 1i*((2*pi*70*(t2 - 250)) / 511));
+s3 = zeros(1,Nt);
+s3(200:300) = exp(-1*(((t3 - 250).^2) / 450) + 1i*(2*pi*130*t3/511));
 
-k = mat2gray(img);
-grey = round(k*256);
+s = (s1 + s2 + s3).';
+
+%% Wigner Distribution
+figure(1)
+plot(t, real(x))
+xlabel('time samples'), ylabel('real(x)')
+axis([0 512 -3 3])
+
 figure(2)
-imshow(grey)
+plot(t, real(s))
+xlabel('time samples'), ylabel('real(x)')
+axis([0 512 -3 3])
+
+[wd_x,~,f] = tfrwv(x);
+figure(3)
+f = f*fs;
+imagesc(t, f, wd_x);
+xlabel('Time Samples')
+ylabel('Frequency (Hz)')
+title('WD of signal x(t)')
+axis xy
+
+[wd_s,~,f] = tfrwv(s);
+figure(4)
+imagesc(t, f, wd_s);
+xlabel('Time Samples')
+ylabel('Frequency (Hz)')
+title('WD of signal s(t)')
+axis xy
+
+%% Unit Energy Gaussian Window
+h = gausswin(64+1,1);
+% figure(5)
+% tfrwv(h);
+
+%% STFT
+% stft_x = tfrstft(x, t, Nt, h);
+% figure(6)
+% imagesc(t, ff, abs(stft_x));
+% axis xy
+% xlabel('Time Samples'), ylabel('Frequency (Hz)')
+% axis([1 Nt 0 Nt/2]);
+
+figure(6)
+tfrstft(x, t, Nt, h);
